775947d96d782fce59d386dfa95896dfc98a60c9
ayathree <nobanitaayathree333@gmail.com>
2024-08-24 22:04:42 +0600

    create navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <nav className="relative bg-white shadow dark:bg-gray-800">
+        <nav className="relative bg-black shadow ">
             <div className="container px-6 py-3 mx-auto">
                 <div className="flex flex-col md:flex-row md:justify-between md:items-center">
                     <div className="flex items-center justify-center">
@@ -15,10 +15,10 @@ const Navbar = () => {
                         <div className="hidden mx-10 md:block">
                                 <div className="relative">
                                     
-                                    <input type="text" className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white  border-b-2 border-black dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:outline-none bold-placeholder " placeholder="Search" />
+                                    <input type="text" className="w-full py-2 pl-10 pr-4 text-gray-700 bg-black  border-b-2 border-white  dark:text-gray-300 dark:border-gray-600 focus:outline-none bold-placeholder " placeholder="Search" />
                                     <span className="absolute inset-y-0 right-0 flex items-center pl-3">
                                     <svg className="w-5 h-5 text-gray-400" viewBox="0 0 24 24" fill="none">
-                                            <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+                                            <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
                                         </svg>
                                         
                                     </span>
@@ -29,7 +29,7 @@ const Navbar = () => {
 
                             
                         </div>
-                            <h1 className='text-4xl font-bold lg:ml-52 '>PURE BLISS</h1>
+                            <h1 className='text-4xl text-white font-bold lg:ml-52 '>PURE BLISS</h1>
 
                         <div className="flex lg:hidden ">
                             <button onClick={() => setIsOpen(!isOpen)} type="button" className="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
@@ -49,12 +49,12 @@ const Navbar = () => {
                     <div className={`${isOpen ? 'translate-x-0 opacity-100' : 'opacity-0 -translate-x-full'} absolute inset-x-0 z-20 w-full px-6 py-2 transition-all duration-300 ease-in-out bg-white top-24 dark:bg-gray-800 md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center`}>
                         <div className="flex flex-col md:flex-row md:mx-1">
                             <div className='flex flex-row items-center'>
-                            <a className="my-2 text-sm leading-5 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:mx-4 md:my-0" href="#">WishList </a>
-                            <FaRegHeart />
+                            <a className="my-2 text-sm leading-5 text-white  transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:mx-4 md:my-0" href="#">WishList </a>
+                            <FaRegHeart className='text-white'/>
                             </div>
                             <div className='flex flex-row items-center' >
-                            <a className="my-2 text-sm leading-5 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:mx-4 md:my-0" href="#">Bag </a>
-                            <IoBagOutline />
+                            <a className="my-2 text-sm leading-5 text-white transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:mx-4 md:my-0" href="#">Bag </a>
+                            <IoBagOutline className='text-white' />
                             </div>
                             
                         </div>
@@ -72,16 +72,13 @@ const Navbar = () => {
                     </div>
                 </div>
 
-                <div className="py-3 mt-3 -mx-3 overflow-y-auto whitespace-nowrap scroll-hidden">
-                    <a className="mx-4 text-sm leading-5 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:my-0" href="#">News</a>
-                    <a className="mx-4 text-sm leading-5 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:my-0" href="#">Articles</a>
-                    <a className="mx-4 text-sm leading-5 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:my-0" href="#">Videos</a>
-                    <a className="mx-4 text-sm leading-5 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:my-0" href="#">Tricks</a>
-                    <a className="mx-4 text-sm leading-5 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:my-0" href="#">PHP</a>
-                    <a className="mx-4 text-sm leading-5 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:my-0" href="#">Laravel</a>
-                    <a className="mx-4 text-sm leading-5 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:my-0" href="#">Vue</a>
-                    <a className="mx-4 text-sm leading-5 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:my-0" href="#">React</a>
-                    <a className="mx-4 text-sm leading-5 text-gray-700 transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:my-0" href="#">JavaScript</a>
+                <div className="py-3 lg:mt-20 -mx-3 flex justify-center items-center overflow-y-auto whitespace-nowrap scroll-hidden">
+                    <a className="mx-4 text-xs leading-5 uppercase font-bold text-white transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:my-0" href="#">Home</a>
+                    <a className="mx-4 text-xs leading-5 uppercase font-bold text-white transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:my-0" href="#">Pages</a>
+                    <a className="mx-4 text-xs leading-5 uppercase font-bold text-white transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:my-0" href="#">Shop</a>
+                    <a className="mx-4 text-xs leading-5 uppercase font-bold text-white transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:my-0" href="#">Store</a>
+                    <a className="mx-4 text-xs leading-5 uppercase font-bold text-white transition-colors duration-300 transform dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400 hover:underline md:my-0" href="#">Blog</a>
+                    
                 </div>
             </div>
         </nav>
