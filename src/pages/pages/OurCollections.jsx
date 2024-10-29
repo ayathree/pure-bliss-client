9ae17c1ceec28e3a8ad7e08b30b0cbe449a3a0b7
ayathree <nobanitaayathree333@gmail.com>
2024-10-30 03:38:34 +0600

    create the our collection page

--- src/pages/pages/OurCollections.jsx
+++ src/pages/pages/OurCollections.jsx
@@ -15,6 +15,49 @@ const OurCollections = () => {
                     </h1>
                 </div>
             </div>
+            {/* section 2 */}
+            <div className="grid lg:grid-cols-3 lg:grid-rows-2 grid-cols-1 grid-rows-5 md:grid-cols-2 justify-center items-center gap-8 mt-28 lg:px-20 px-7">
+    {/* Row-span image takes the full height of two rows */}
+    <div className=" md:col-span-2 md:row-span-1   lg:row-span-2 lg:col-span-1  h-full w-full">
+        <img
+            className="h-full w-full object-cover"
+            src="https://i.ibb.co/FwMFzSP/161007137-818106462117352-2772674974349771071-n.jpg"
+            alt=""
+        />
+    </div>
+    
+    {/* Smaller images filling half of each row */}
+    <div className="h-full w-full">
+        <img
+            className="h-full w-full object-cover"
+            src="https://i.ibb.co/FwMFzSP/161007137-818106462117352-2772674974349771071-n.jpg"
+            alt=""
+        />
+    </div>
+    <div className="h-full w-full">
+        <img
+            className="h-full w-full object-cover"
+            src="https://i.ibb.co/FwMFzSP/161007137-818106462117352-2772674974349771071-n.jpg"
+            alt=""
+        />
+    </div>
+    <div className="h-full w-full">
+        <img
+            className="h-full w-full object-cover"
+            src="https://i.ibb.co/FwMFzSP/161007137-818106462117352-2772674974349771071-n.jpg"
+            alt=""
+        />
+    </div>
+    <div className="h-full w-full">
+        <img
+            className="h-full w-full object-cover"
+            src="https://i.ibb.co/FwMFzSP/161007137-818106462117352-2772674974349771071-n.jpg"
+            alt=""
+        />
+    </div>
+</div>
+
+            <div></div>
         </div>
     );
 };
