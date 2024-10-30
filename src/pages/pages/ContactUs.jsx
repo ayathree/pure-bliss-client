e2d108313322f229fda61f678bc1215138b32452
ayathree <nobanitaayathree333@gmail.com>
2024-10-31 03:12:35 +0600

    create the contact us page section 1 and 2

--- src/pages/pages/ContactUs.jsx
+++ src/pages/pages/ContactUs.jsx
@@ -16,14 +16,25 @@ const ContactUs = () => {
                 Lorem ipsum dolor sit amet condui tincidu mauris porttitor, varius parris@qodeinteractive.com tellus sumt 2-220-330-880 luctus nec ullamcorper mattis, pulvinar dapibus leo dolor sit amet.
                 </p>
                 <form className="mt-8">
-                    <input type="text" className="border-b-2 focus:border-black border-gray-400 focus:outline-none w-full mt-8" placeholder="Name" />
-                    <input type="email" className="border-b-2 focus:border-black border-gray-400 focus:outline-none w-full mt-8" placeholder="Email" />
-                    <input type="text" className="border-b-2 focus:border-black border-gray-400 focus:outline-none w-full mt-8" placeholder="Phone" />
-                    <textarea className="border-b-2 focus:border-black  border-gray-400 focus:outline-none w-full mt-8" name="" id="" placeholder="Comment"></textarea>
+                    <input type="text" className="border-b focus:border-black border-gray-400 focus:outline-none w-full mt-8" placeholder="Name" />
+                    <input type="email" className="border-b focus:border-black border-gray-400 focus:outline-none w-full mt-8" placeholder="Email" />
+                    <input type="text" className="border-b focus:border-black border-gray-400 focus:outline-none w-full mt-8" placeholder="Phone" />
+                    <textarea className="border-b focus:border-black  border-gray-400 focus:outline-none w-full mt-8" name="" id="" placeholder="Comment"></textarea>
                     <button className="btn btn-outline hover:bg-black font-semibold uppercase mt-8 px-16 text-black rounded-none">contact us</button>
                 </form>
             </div>
             </div> 
+            {/* section2 */}
+            <div className="mt-28">
+                <p className="text-3xl font-bold uppercase text-center px-28" >GET $10 OFF YOUR FIRST PURCHASE! <br />
+                SIGN UP FOR EMAIL UPDATES ON LAUNCHES, RESTOCKS & MORE!</p>
+                <form className="px-64 mt-8">
+                <input type="text" className="border-b text-center  focus:border-black border-gray-400 focus:outline-none w-full mt-8" placeholder="Your email..." />
+                <div className="flex justify-center">
+                <button className="btn btn-outline hover:bg-black font-semibold uppercase mt-8 f px-16 text-black rounded-none">subscribe now</button>
+                </div>
+                </form>
+            </div>
         </div>
     );
 };
