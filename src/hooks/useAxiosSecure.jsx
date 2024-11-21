18802efc12590764825d8968753501b94887f67a
ayathree <nobanitaayathree333@gmail.com>
2024-11-22 02:11:45 +0600

    create the axiosSecure method

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,11 +1,32 @@
+import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
+export const axiosSecure=axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
+})
 
 const useAxiosSecure = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+    const {logOut}=useAuth()
+    const navigate=useNavigate()
+    useEffect(() => {
+        axiosSecure.interceptors.response.use (
+            res=>{
+                return res
+            },
+            async error=>{
+                console.log('error tracked in the interceptors', error.response)
+                if (error.response.status===401 || error.response.status===403){
+                    await logOut()
+                navigate('/myAccount')
+            }
+            return Promise.reject(error)
+        
+    }
+)}, [logOut, navigate])
+   return axiosSecure 
+}
 
 export default useAxiosSecure;
